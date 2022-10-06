80a356ca4d5dbfe84e5337dea47eaec57ddb40ef
Ghedo44 <federicoghedini@gmail.com>
2022-10-06 16:19:21 +0200

    cose

--- js/editor.js
+++ js/editor.js
@@ -1,7 +1,6 @@
 const editor = document.querySelector("#latex_editor");
 
 
-
 const applyLatex = (id, remplaceText=true) => {
     let first = latexCodes[id]; 
     let last = latexCodes[id+1];
@@ -20,3 +19,27 @@ const applyLatex = (id, remplaceText=true) => {
         editor.setRangeText(`${first}${last}`, selectionStart, selectionEnd, 'end');
     }
 }
+
+// Tab skip to next braces
+window.addEventListener('keydown', function(event) {
+    const key = event.key;
+    if(key === 'Tab') {
+        event.preventDefault();
+        nextPosition('{');
+      }
+  });
+
+function nextPosition(location) {
+    let currentCaretPosition = editor.selectionStart;
+    editor.focus();
+    
+    let nextWordPosition = editor.value.indexOf(location, currentCaretPosition) + 1;
+
+    if (nextWordPosition < currentCaretPosition) {
+        const end = editor.value.length;
+        editor.setSelectionRange(end, end);
+    } else {
+        editor.setSelectionRange(nextWordPosition, nextWordPosition);
+    }
+  }
+  
