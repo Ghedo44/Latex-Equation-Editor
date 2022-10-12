2bce89ec725f6eaf25d65727a9bbf511c43b73e6
AndromedaM31X <87262523+AndromedaM31X@users.noreply.github.com>
2022-10-13 00:01:48 +0200

    new button creation
    Clicking the + button will open a vertical menu. By clicking an item in the menu it will add it under the textarea for quick access.
    Note: Values are still to be inserted

--- docs/js/create.js
+++ docs/js/create.js
@@ -2,19 +2,29 @@
 
 
 
-function NewBtn(id, value){
+function NewBtn(id){
 
     
     var btn= document.createElement("button");
-    var number = parseInt(id)
+    var number = parseInt(id);
     
-    btn.innerHTML = value
+    btn.innerHTML = document.getElementById(id).value
+    
+    btn.setAttribute("id", id);
+    btn.style.width="50px"
     
     
-    var body = document.getElementById("btns");
+    var body = document.getElementById("testing");
         body.appendChild(btn);
     
-    btn.addEventListener('click',LaunchLatex(number))
+    
+    console.log(number, id );
+    
+
+   
+
+    
+        
 
    
 }   
@@ -34,7 +44,17 @@ function OpenMenu(){
 
 }
 
-function LaunchLatex(number){
 
-    btn.addEventListener('click', applyLatex(number));
-}
+document.getElementById('testing').addEventListener('click', function(e){
+
+    if(e.target && e.target.nodeName == "BUTTON"){
+
+        var getId = e.target.id
+        var IntID = parseInt(getId)
+        console.log("Button", e.target.id.replace("tt"), "clicked")
+        console.log(getId)
+        applyLatex(IntID)
+        
+    }
+
+})
